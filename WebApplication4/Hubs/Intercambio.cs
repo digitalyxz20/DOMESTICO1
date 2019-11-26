1e519c89c3fd7ab2364e65cd28206187154cb2d6
javier yucra yauri <javier_mejor20@hotmail.com>
2019-11-26 15:44:43 -0500

    iniciando proyecto

---
+++ WebApplication4/Hubs/Intercambio.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Hubs
+{
+    public class Intercambio: Hub
+    { 
+        public async Task MensajeEnviado(string user, string mensaje)
+        {
+            Console.WriteLine("mensaje enviado con" + mensaje);
+            await Clients.Others.SendAsync("EnviandoMensaje", user, mensaje);
+        }
+    }
+}
